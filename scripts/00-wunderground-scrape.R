7a1219fb932cbf4b66202023f20bf967eba8faa9
Charlotte Mann <manncz@umich.edu>
2022-11-08 16:42:50 -0500

    add function to summarize data pulled from wunderground

--- scripts/00-wunderground-scrape.R
+++ scripts/00-wunderground-scrape.R
@@ -5,7 +5,7 @@
 ##'
 #+ echo=FALSE, include = FALSE
 knitr::opts_chunk$set(warning=FALSE, echo=TRUE)
-mypacks <- c("dplyr","curl", "jsonlite")  # what packages are needed?
+mypacks <- c("dplyr","curl", "jsonlite", "lubridate")  # what packages are needed?
 lapply(mypacks, library, character.only=TRUE)  # load all packages
 
 ##' Adapted from script by Yash Patel found [here](https://github.com/yashpatel5400/stats604-project4/blob/main/raw_data/wunderground_download.py)
@@ -37,12 +37,37 @@ fetch_wunderground <- function(stationid = "KPDX", start_date = "20221001", end_
   return(json.dat$observations)
 }
 
+summarize_wunderground <- function(raw.table){
+  
+  stopifnot("temp" %in% colnames(raw.table),
+            "valid_time_gmt" %in% colnames(raw.table))
+  
+  #wunderground includes the temperatures starting from 8am on the date of interest until
+  #8am of the following calendar day
+  
+  dat <- raw.table %>%
+    select(valid_time_gmt, temp) %>%
+    mutate(date_time = as_datetime(valid_time_gmt),
+           day = date(date_time),
+           tod = hour(date_time)) %>%
+    mutate(date_group = case_when(tod < 8 ~ day - days(1),
+                                  TRUE ~ day)) %>%
+    group_by(date_group) %>%
+    summarize(min.temp = min(temp), max.temp = max(temp), mean.temp = mean(temp))
+  
+  return(dat)
+  
+}
+
+
 
 ##' Now, let's test it out
-weather.dat <- fetch_wunderground()
+weather.dat <- fetch_wunderground(start_date = "20221106", end_date = "20221107")
 
 weather.dat %>%
   knitr::kable(format = "markdown")
+
+summarize_wunderground(weather.dat)
 
 
 
